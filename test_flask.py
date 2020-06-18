67508fbc67c3de3ef0063032586e4edcdbccd39d
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-06-18 16:56:05 -0700

    testing

--- test_flask.py
+++ test_flask.py
@@ -65,43 +65,42 @@ class UserViewsTestCase(TestCase):
 
 # ********************* doesn't work  ************
 
-    # def test_create_user(self):
-    #     '''Test valid route, create user, save to db, and POST request.'''
-    #     # form data
-    #
-    #     with self.client as client:
-    #         # test with None image url
-    #         form = {
-    #             "first_name": "TestFirstName",
-    #             "last_name": "TestLastName",
-    #             "image_url": None,
-    #         }
-    #
-    #         # query the database for the user
-    #         user = User.query.first()
-    #         # assert that the info in the database matches the info in the form above
-    #         self.assertTrue(user.first_name == form["first_name"])
-    #         self.assertTrue(user.last_name == form["last_name"])
-    #
-    #         # default_image_url = "https://upload-icon.s3.us-east-2.amazonaws.com/uploads/icons/png/16671574911586787867-512.png"
-    #         # self.assertTrue(user.image_url == default_image_url)
-    #         # self.assertTrue(user.image_url == 'https://upload-icon.s3.us-east-2.amazonaws.com/uploads/icons/png/16671574911586787867-512.png')
-    #
-    #
-    #         # test with image url
-    #         form = {
-    #             "first_name": "Test",
-    #             "last_name": "Test",
-    #             "image_url": "https://upload-icon.s3.us-east-2.amazonaws.com/uploads/icons/png/16671574911586787867-512.png",
-    #         }
-    #         response = client.post("/create_user", data=form)
-    #         self.assertTrue(response.status_code == 200)
-    #         self.assertTrue(user.first_name == form["first_name"])
-    #         self.assertTrue(user.last_name == form["last_name"])
-    #         self.assertTrue(user.image_url == form['https://upload-icon.s3.us-east-2.amazonaws.com/uploads/icons/png/16671574911586787867-512.png'])
-    #
-    #         response = client.post("/create_user", data=form)
-    #         self.assertTrue(response.status_code == 302)
+    def test_create_user(self):
+        '''Test valid route, create user, save to db, and POST request.'''
+        # form data
+
+        with self.client as client:
+            # test with None image url
+            form = {
+                "first_name": "TestFirstName",
+                "last_name": "TestLastName",
+                "image_url": None,
+            }
+
+            # query the database for the user
+            user = User.query.first()
+            # assert that the info in the database matches the info in the form above
+            self.assertTrue(user.first_name == form["first_name"])
+            self.assertTrue(user.last_name == form["last_name"])
+
+            # default_image_url = "https://upload-icon.s3.us-east-2.amazonaws.com/uploads/icons/png/16671574911586787867-512.png"
+            # self.assertTrue(user.image_url == default_image_url)
+            # self.assertTrue(user.image_url == 'https://upload-icon.s3.us-east-2.amazonaws.com/uploads/icons/png/16671574911586787867-512.png')
+
+
+            # test with image url
+            form = {
+                "first_name": "Test",
+                "last_name": "Test",
+                "image_url": "https://upload-icon.s3.us-east-2.amazonaws.com/uploads/icons/png/16671574911586787867-512.png",
+            }
+
+            self.assertTrue(user.first_name == form["first_name"])
+            self.assertTrue(user.last_name == form["last_name"])
+            self.assertTrue(user.image_url == form['https://upload-icon.s3.us-east-2.amazonaws.com/uploads/icons/png/16671574911586787867-512.png'])
+
+            response = client.post("/create_user", data=form)
+            self.assertTrue(response.status_code == 302)
 
 
     def test_edit_user(self):
@@ -112,3 +111,37 @@ class UserViewsTestCase(TestCase):
             response = client.get(f'/users/{self.user_id}/edit', user=user, follow_redirects=True)
             self.assertTrue(response.status_code == 200)
 
+    def test_update_user(self):
+        '''Test valid route, POST method, collect for data and save to db.'''
+        with self.client as client:
+
+            # test with None image url
+            form = {
+                "first_name": "TestFirstName",
+                "last_name": "TestLastName",
+                "image_url": None,
+            }
+
+            user = User.query.first()
+
+            self.assertTrue(user.first_name == form["first_name"])
+            self.assertTrue(user.last_name == form["last_name"])
+            # default_image_url = "https://upload-icon.s3.us-east-2.amazonaws.com/uploads/icons/png/16671574911586787867-512.png"
+            # self.assertTrue(user.image_url == default_image_url)
+            # self.assertTrue(user.image_url == 'https://upload-icon.s3.us-east-2.amazonaws.com/uploads/icons/png/16671574911586787867-512.png')
+
+            # test with image url
+            form = {
+                "first_name": "Test",
+                "last_name": "Test",
+                "image_url": "https://upload-icon.s3.us-east-2.amazonaws.com/uploads/icons/png/16671574911586787867-512.png",
+            }
+
+            self.assertTrue(user.first_name == form["first_name"])
+            self.assertTrue(user.last_name == form["last_name"])
+            self.assertTrue(user.image_url == form['https://upload-icon.s3.us-east-2.amazonaws.com/uploads/icons/png/16671574911586787867-512.png'])
+
+            response = client.post(f"/users/{user_id}/edit", data=form)
+            self.assertTrue(response.status_code == 200)
+
+    def
